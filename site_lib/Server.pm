1f8efc7b0a6d78dee694c804c3c400d8a4dac5f1
bjoerndalen <bjoerndalen.93@mail.ru>
2014-03-12 12:48:23 +0200

    Add extending of Net::Server::Fork
    Add extending of Net::Server::Fork class for
    main Server class. Also add method, that should be
    overridden.

--- site_lib/Server.pm
+++ site_lib/Server.pm
@@ -1,6 +1,40 @@
-#!/usr/bin/perl
-
+package Server;
 use strict;
 use warnings;
+use Mouse;
+use AndroidServer;
+use AndroidServer::Teacher;
+
+extends 'Net::Server::Fork';
+
+has 'registred_user' => (
+	is       => 'rw',
+	isa      => 'HashRef',
+	required => 0,
+	default  => sub { return {}; },
+	lazy     => 1,
+	reader   => 'get_reg_users',
+	writer   => 'set_reg_users',
+);
+
+has 'teacher_server' => (
+	is       => 'rw',
+	isa      => 'Any',
+	required => 0,
+	default  => sub { return AndroidServer::Teacher->new(); },
+	lazy     => 1,
+	reader   => 'get_teacher_serv',
+	writer   => 'set_teacher_serv',
+);
+
+# Overriden method
+sub process_request {
+	my $self = shift;
+	while (<STDIN>) {
+
+		# TODO processing
+	}
+}
 
 # TODO Main Server Class
+1;
